a3997076adafd5fb553c46e87118c6892a1bcbdf
yaraelzein <yaraelzein2001@gmail.com>
2022-02-21 12:58:16 -0500

    sort

--- src/data.js
+++ src/data.js
@@ -1,21 +1,55 @@
 // import data from "./data/ghibli/ghibli.js";
 
 export const filterRanking = (data) => {  
-  let arrayTop = data.filter(film => film.rt_score >= 83);
+  let arrayTop = data.filter(films => films.rt_score >= 83);
   arrayTop.length = 10;
   return arrayTop;
 
  
 };
 
-
-
 export const getPersonajes = (title, data) => {
-  let lengthPersonajes = data.films.length
-  
-  for( let i = 0; i < lengthPersonajes; i++){
-    if( data.films[i].title == title){
-      return data.films[i].people
-    }
-  }
+  // let lengthPersonajes = data.films.length
+  data.filter(films => {
+    return films.title == title ? films.people : null
+  })
+
 };
+
+export const peopleOrden = (data) => {
+  console.log(data)
+    let arrayPeople = []
+    data.forEach((element) => {
+      arrayPeople.push(element.people)
+    });
+    // arrayPeopleS.array.forEach((people, index) => {
+    //   return pe    
+    // });
+    console.log(".people", arrayPeople)
+    let ordenado = arrayPeople.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    })
+    console.log("ordenado", ordenado)
+  //   if (peopleOrden === "Ascendente A-Z"){
+  //     return arrayPeopleS.sort();
+  //   }  else if (peopleOrden === "Descendente Z-A"){
+  //     return arrayPeopleS.reverse();
+  //   }
+      
+   };
+
+
+
+// export const ordenadorA-Z = (people) => {
+//   people.sort();
+//    console.log(people.sort());
+// }
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import data from "./data/ghibli/ghibli.js"
-import {filterRanking} from "./data.js"
-
+import {filterRanking, peopleOrden} from "./data.js"
+const peliculasData = data.films;
 
 // reload button HOME
 
@@ -12,75 +12,77 @@ document.getElementById("main__header-options-boton-reload").addEventListener("c
 
 // Mensaje de Bienvenida
 export function Saludo (){
-let welcome = document.createElement('div');
-welcome.createElement("div");
-welcome.innerHTML = `<div>
-<p> Bienvenidos a la fan Pages de Studio Ghibli</p>
-</div>`;
-
-document.body.appendChild(welcome);
+  let welcome = document.createElement('div');
+  welcome.createElement("div");
+  welcome.innerHTML = `<div>
+  <p> Bienvenidos a la fan Pages de Studio Ghibli</p>
+  </div>`;
+  document.body.appendChild(welcome);
 }
 
-export let peliculas = document.getElementById("main__header--options-boton-json");
+export let peliculas = document.getElementById("main__header--options-button-json");
 const lista = document.getElementById("main__body--peliculas-cards");
+const seccionPelicula = document.getElementById("seccion-peliculas");
+const seccionPersonajes = document.getElementById("seccion-personajes");
+const orderCheck = document.getElementById("orderCheck")
+const peliculaSeleccionada = document.getElementById("main__card--peliculas-select");
+
+
 peliculas.addEventListener("click", () => {
-  let peliculasData = data.films;
+  
   let template = "";
+ 
   peliculasData.forEach((obj, index) => {
     template += `
-            <div class='prueba'>
-              <img id='images' src='${obj.poster}'> 
-              <p id = "">${obj.title}</p>
-              <p>${obj.release_date}</p>
-              <button id='details-${index}'>Detalles</button>
-            </div>`;
+    <div id='prueba'>
+    <img id='images' src='${obj.poster}'> 
+    <p id = "">${obj.title}</p>
+    <p>${obj.release_date}</p>
+    <button id='details-${index}'>Detalles</button>
+    </div>`;
   });
   lista.innerHTML = template; 
   // puedo ir por el boton y asignarle un evento
-  
-peliculasData.map(function(film, index){
-  console.log(film,index);
-    const detallesPelicula = document.getElementById('details-'+index);
+  peliculasData.map(function(film, index){
+    // console.log(film,index);
+    const detallesPelicula = document.getElementById('details-' + index);
     detallesPelicula.addEventListener('click', ()=>{
-     
-      let x = film.people;
-      x.forEach((people) =>{
-        document.getElementById("list-personaje").innerHTML += 
-      `<div>
-          <p>${people.name}</p>
-      <img src="${people.img}">
-      </div>
-      `;
+      let getPeople = film.people;
+      // oculta peliculas menu
+      lista.style.display = "none"
+      // muestra seccion personajes
+      orderCheck.style.display= "inline"
+      seccionPersonajes.style.display = "inline"
+      seccionPelicula.style.display = "inline"
+      peliculaSeleccionada.innerHTML += `
+        <img src="${film.poster}">
+        <p>
+          <b>TITLE:</b> ${film.title} <br>
+          <b>DESCRIPTION:</b> ${film.description} <br>
+          <b>DIRECTOR:</b> ${film.director} <br>
+          <b>PRODUCER:</b> ${film.producer} <br>
+        </p>
+      `
+
+      getPeople.forEach((people) => {
+      document.getElementById("list-personaje").innerHTML += `
+        <div id="prueba" class="prueba-personajes">
+          <p>
+          <b> Nombre: </b> ${people.name}<br>
+          <img  id="images" src="${people.img}"><br>
+          <b> Genero: </b> ${people.gender}<br>
+          <b> Edad: </b> ${people.age}<br>
+          <b> Especie: </b> ${people.specie}</br>
+          </p>
+        </div>`;
       })
-      
-      
     });
   });
 });
 
+// top 10 peliculas
 
-
-
-
-//   peliculasData.forEach((film, index) => {
-//     const detallesPelicula = document.getElementById('details-'+index);
-//     detallesPelicula.addEventListener('click', ()=>{
-//       console.log(film);
-      
-//       document.getElementById("list-personaje").textContent += 
-//       `<div>
-//           <li>${film.people[index]}</li>
-//       <img src="${film.people[index]}"> </div>
-//       `;
-      
-//     });
-//   });
- 
-
-
-
-
-let ranking = document.getElementById("main__header-options-boton-ranking");
+let ranking = document.getElementById("main__header-options-button-ranking");
 const tRanking = document.getElementById("main__body-raking");
 const visibilitypeliculas =  document.getElementById("main__body--peliculas-cards");
 // console.log(data)
@@ -98,60 +100,27 @@ ranking.addEventListener("click", () => {
   visibilitypeliculas.style.display = "none"
 });
 
-
-
-//  personajes
-// let detallePelicula = document.getElementById('details');
 // let personajeLista = document.getElementById('list-personaje')
 // // let divPersonajes = document.querySelector(".list-personaje");
-// detallePelicula.addEventListener('click', ()=>{
 
-//   let peoplesTotal = filterPeople(data.films);
-//   peoplesTotal.forEach((obj) => {
-//     personajeLista.innerHTML += `<div>
-//     <li>${obj.name}</li>
-// <img src="${obj.img}"> </div>
-// `;
-
-//   })
-// });
-
-
-
-// let divPersonajes = document.getElementById("div-personajes");
-//   document.getElementById('personajes').addEventListener('click', ()=>{
-//     let personajes = getPersonajes("Castle in the Sky", data)
-//     // console.log(personajes);    
-//     personajes.forEach(element =>{  
-//         divPersonajes.innerHTML += `
-//             <li>${element.name}</li>
-//             <img src="${element.img}">    
-//         `
-//     })
-//   });
-
-
-// // prueba checkbox
-// let fromAToZ = document.getElementById("from_A-Z")
-// let fromZToA = document.getElementById("from_Z-A")
-// let personajesPonyo = ["sosuke", "lisa", "kumiko", "ponyo"];
 
 // fromAToZ.addEventListener("click", () => {
-//     personajesPonyo.sort();
-//     console.log(personajesPonyo);
-
+//   personajesPonyo.sort();;
+//   console.log(personajesPonyo);;
+// // 
     
-//     fromZToA.disabled = true;
+//   fromZToA.disabled = true;
 // })
 
 // fromZToA.addEventListener("click", () => {
 //     personajesPonyo.sort();
 
-//     let personajesPonyoFromAToZ = [];
+//   let personajesPonyoFromAToZ = [];
 //     personajesPonyoFromAToZ = personajesPonyo.sort();
 
-//     personajesPonyoFromAToZ.reverse();
+//   personajesPonyoFromAToZ.reverse();
 //     console.log(personajesPonyoFromAToZ);
 
-//     fromAToZ.disabled = true
+//   fromAToZ.disabled = trues
 // })
+peopleOrden(peliculasData)
